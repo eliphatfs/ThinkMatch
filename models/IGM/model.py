daa8097ebbea70b8ca56ba785ff84333aa6f7e6c
eliphatfs <flandre@scarletx.cn>
2021-11-24 22:43:50 +0800

    Transpose

--- models/IGM/model.py
+++ models/IGM/model.py
@@ -51,9 +51,8 @@ class Net(nn.Module):
         if 'gt_perm_mat' in data_dict:
             align = data_dict['gt_perm_mat'].argmax(-1)
             y_tgt_rand = y_tgt[..., torch.randperm(y_tgt.shape[-1]).to(align)]
-            y_tgt_pm = y_tgt[torch.arange(len(y_tgt)).unsqueeze(1).to(align), ..., align]
-            import pdb; pdb.set_trace()
-            data_dict['loss'] = F.kl_div(y_src[..., :y_tgt_pm.shape[-1]], y_tgt_pm) - F.kl_div(y_src, y_tgt_rand)
+            y_tgt_pm = y_tgt[torch.arange(len(y_tgt)).unsqueeze(1).to(align), ..., align].transpose(-1, -2)
+            data_dict['loss'] = F.kl_div(y_src, y_tgt_pm) - F.kl_div(y_src, y_tgt_rand)
         lab_src = y_src.argmax(1)
         lab_tgt = y_tgt.argmax(1)
         perm_mat = torch.zeros([*y_tgt.shape, y_tgt.shape[-1]]).to(y_tgt)
