d3f6d79ac16556230eb056be32c5e039415c8f17
eliphatfs <flandre@scarletx.cn>
2021-11-25 19:27:54 +0800

    Debug, 4 attempts and counting

--- models/IGM/model.py
+++ models/IGM/model.py
@@ -71,9 +71,8 @@ class Net(nn.Module):
         data_dict['ds_mat'] = sim  # self.sinkhorn(sim, ns_src, ns_tgt, dummy_row=True)
         data_dict['perm_mat'] = hungarian(data_dict['ds_mat'], ns_src, ns_tgt)
         if 'gt_perm_mat' in data_dict:
-            if random.random() < 0.04:
-                print(sim[0])
-                print(data_dict['gt_perm_mat'][0])
+            print(sim[0])
+            print(data_dict['gt_perm_mat'][0])
             data_dict['loss'] = F.binary_cross_entropy(sim, data_dict['gt_perm_mat'])
         # if 'gt_perm_mat' in data_dict:
         #     align = data_dict['gt_perm_mat'].argmax(-1)
