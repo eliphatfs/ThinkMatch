1fa7bd5c72ea8d80ed7adfe0b10f482fd7045953
eliphatfs <flandre@scarletx.cn>
2021-11-26 10:27:06 +0800

    Modifications

--- models/IGM/model.py
+++ models/IGM/model.py
@@ -3,7 +3,6 @@ from torchvision.models import resnet50
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from src.feature_align import feature_align
 from src.utils.config import cfg
 from src.lap_solvers.hungarian import hungarian
 import random
@@ -25,6 +24,16 @@ class ResCls(nn.Module):
             d = chorus(d)
             x = x + d
         return self.outro(x)
+
+
+def my_align(raw_feature, P, ns_t, ori_size: tuple):
+    return F.grid_sample(
+        raw_feature,
+        2 * P.unsqueeze(-2) / ori_size[0] * raw_feature.shape[-1] - 1,
+        'bicubic',
+        'border',
+        align_corners=False
+    ).squeeze(-1)
 
 
 class Net(nn.Module):
@@ -51,6 +60,7 @@ class Net(nn.Module):
         x = r.layer1(x)
         x = r.layer2(x)
         g = r.layer3(x)
+        g = r.layer4(g)
         g = r.avgpool(g).flatten(1).unsqueeze(-1)
         return g, x
 
@@ -60,8 +70,8 @@ class Net(nn.Module):
         ns_src, ns_tgt = data_dict['ns']
         glob_src, feat_src = self.encode(src)
         glob_tgt, feat_tgt = self.encode(tgt)
-        U_src = feature_align(feat_src, P_src, ns_src, self.rescale)
-        U_tgt = feature_align(feat_tgt, P_tgt, ns_tgt, self.rescale)
+        U_src = my_align(feat_src, P_src, ns_src, self.rescale)
+        U_tgt = my_align(feat_tgt, P_tgt, ns_tgt, self.rescale)
         F_src = torch.cat([
             U_src,
             glob_src.expand(*glob_src.shape[:-1], U_src.shape[-1]),
@@ -74,8 +84,8 @@ class Net(nn.Module):
         ], 1)
         y_src = self.cls(F_src)
         y_tgt = self.cls(F_tgt)
-        sim = torch.einsum("bci,bcj->bij", y_src, y_tgt)
-        data_dict['ds_mat'] = self.sinkhorn(sim, ns_src, ns_tgt, dummy_row=True)
+        sim = torch.sigmoid(torch.einsum("bci,bcj->bij", y_src, y_tgt))
+        data_dict['ds_mat'] = sim  # self.sinkhorn(sim, ns_src, ns_tgt, dummy_row=True)
         data_dict['perm_mat'] = hungarian(data_dict['ds_mat'], ns_src, ns_tgt)
         loss = 0.0
         if 'gt_perm_mat' in data_dict:
