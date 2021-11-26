a67adf6b925b119b4a8d4c85ca6c35deac5814b6
eliphatfs <flandre@scarletx.cn>
2021-11-26 17:23:11 +0800

    Debug again

--- extra/perspective.py
+++ extra/perspective.py
@@ -91,10 +91,10 @@ class RandomPerspective(torch.nn.Module):
             yn = (d * x + e * y + f) / (g * x + h * y + 1)
             pn = torch.stack([yn, xn], -1)
             q = F.perspective(img, startpoints, endpoints, Image.BICUBIC, fill)
-            q[yn, xn] = q.max()
-            q[yn + 1, xn + 1] = q.max()
-            q[yn + 1, xn] = q.max()
-            q[yn, xn + 1] = q.max()
+            q[yn.long(), xn.long()] = 255
+            q[yn.long() + 1, xn.long() + 1] = 255
+            q[yn.long() + 1, xn.long()] = 255
+            q[yn.long(), xn.long() + 1] = 255
             q.save("test.png")
             import pdb; pdb.set_trace()
             return F.perspective(img, startpoints, endpoints, Image.BICUBIC, fill), pn
