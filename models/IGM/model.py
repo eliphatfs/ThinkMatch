f163481beeefba4ab904ab898faf45c851568135
eliphatfs <flandre@scarletx.cn>
2021-11-26 15:13:46 +0800

    Fix 2

--- models/IGM/model.py
+++ models/IGM/model.py
@@ -100,11 +100,10 @@ class Net(nn.Module):
 
     def attn(self, y_src, y_tgt, P_src, P_tgt, n_src, n_tgt):
         exp_posemb = self.pos_emb.expand(len(y_src), *self.pos_emb.shape)
-        y_src = (y_src + my_align(exp_posemb, P_src, self.rescale)).permute(2, 0, 1)
-        y_tgt = (y_tgt + my_align(exp_posemb, P_tgt, self.rescale)).permute(2, 0, 1)
-        import pdb; pdb.set_trace()
         key_mask_src = torch.arange(y_src.shape[-1]).expand(len(y_src), y_src.shape[-1]) < n_src.unsqueeze(-1)
         key_mask_tgt = torch.arange(y_tgt.shape[-1]).expand(len(y_tgt), y_tgt.shape[-1]) < n_tgt.unsqueeze(-1)
+        y_src = (y_src + my_align(exp_posemb, P_src, self.rescale)).permute(2, 0, 1)
+        y_tgt = (y_tgt + my_align(exp_posemb, P_tgt, self.rescale)).permute(2, 0, 1)
         for atn, ff in zip(self.attentions, self.atn_mlp):
             atn_src, _ = atn(y_src, y_tgt, y_tgt, key_padding_mask=key_mask_tgt)
             atn_tgt, _ = atn(y_tgt, y_src, y_src, key_padding_mask=key_mask_src)
