6d786d065a2e4239fa2772a29d59f79c062ca7d6
eliphatfs <flandre@scarletx.cn>
2021-11-28 18:04:25 +0800

    BACK TO HALO

--- models/IGM/model.py
+++ models/IGM/model.py
@@ -1,4 +1,4 @@
-from models.IGM.efficientnet import efficientnet_b4
+from torchvision.models import resnet34
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -39,16 +39,15 @@ def my_align(raw_feature, P, ori_size: tuple):
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.resnet = efficientnet_b4()  # UNet(3, 2)
-        self.resnet.load_state_dict(torch.load("efficient_b4_pretrained.pt"))
+        self.resnet = resnet34(True)  # UNet(3, 2)
         # self.unet.load_state_dict(torch.load("unet_carvana_scale0.5_epoch1.pth"))
-        feature_lat = 4736
-        self.cls = ResCls(2, feature_lat, 2048, 1024)
+        feature_lat = 64 + (64 + 128 + 256 + 512 + 512 * 2)
+        self.cls = ResCls(1, feature_lat, 2048, 1024)
         self.tau = cfg.NGM.SK_TAU
         self.rescale = cfg.PROBLEM.RESCALE
-        self.pf = ResCls(1, 3, 128, 256)
-        self.pn = ResCls(1, (1024 + 256) * 2, 2048, 2048, 2)
-        self.pe = ResCls(2, 2048 + (1024 + 256) * 2, 2048, 1, 2)
+        self.pf = ResCls(0, 4, 128, 256)
+        self.pn = ResCls(1, 1024 + 256, 1536, 1536, 2)
+        self.pe = ResCls(1, 2048 + 1024 + 256, 2048, 64, 2)
         self.sinkhorn = Sinkhorn(
             max_iter=cfg.NGM.SK_ITER_NUM, tau=self.tau, epsilon=cfg.NGM.SK_EPSILON
         )
@@ -66,14 +65,24 @@ class Net(nn.Module):
         x = r.avgpool(x)
         yield x
 
-    def align(self, feat_srcs, feat_tgts, P_src, P_tgt):
+    def halo(self, feat_srcs, feat_tgts, P_src, P_tgt):
         U_src = torch.cat([
             my_align(feat_src, P_src, self.rescale) for feat_src in feat_srcs
         ], 1)
         U_tgt = torch.cat([
             my_align(feat_tgt, P_tgt, self.rescale) for feat_tgt in feat_tgts
         ], 1)
-        return U_src, U_tgt
+        glob_src = feat_srcs[-1].flatten(1).unsqueeze(-1)
+        glob_tgt = feat_tgts[-1].flatten(1).unsqueeze(-1)
+        F_src = torch.cat([
+            U_src,
+            glob_tgt.expand(*glob_tgt.shape[:-1], U_src.shape[-1])
+        ], 1)
+        F_tgt = torch.cat([
+            U_tgt,
+            glob_src.expand(*glob_src.shape[:-1], U_tgt.shape[-1])
+        ], 1)
+        return F_src, F_tgt
 
     def points(self, y_src, y_tgt, P_src, P_tgt, n_src, n_tgt):
         resc = P_src.new_tensor(self.rescale)
@@ -84,19 +93,14 @@ class Net(nn.Module):
             P_tgt = P_tgt + torch.randn_like(P_tgt)[..., :1] * 0.1
         key_mask_src = torch.arange(y_src.shape[-1], device=n_src.device).expand(len(y_src), y_src.shape[-1]) < n_src.unsqueeze(-1)
         key_mask_tgt = torch.arange(y_tgt.shape[-1], device=n_tgt.device).expand(len(y_tgt), y_tgt.shape[-1]) < n_tgt.unsqueeze(-1)
-        P_src = self.pf(torch.cat((P_src, torch.zeros_like(P_src[:, :1])), 1))
-        P_tgt = self.pf(torch.cat((P_tgt, torch.ones_like(P_tgt[:, :1])), 1))
-        yc_src = torch.cat((P_src, y_src), 1)
-        yc_tgt = torch.cat((P_tgt, y_tgt), 1)
-        
-        e_src = torch.cat((yc_src, torch.zeros_like(yc_src)), 1).unsqueeze(-1)
-        e_tgt = torch.cat((torch.zeros_like(yc_tgt), yc_tgt), 1).unsqueeze(-2)
-        pcd = e_src + e_tgt
-        mask = key_mask_src.unsqueeze(-1) & key_mask_tgt.unsqueeze(-2)
-
-        pcc = (self.pn(pcd) * mask.unsqueeze(1)).flatten(2).max(-1)[0].unsqueeze(-1).unsqueeze(-1)
-        pcc_b = pcc.expand(pcc.shape[0], pcc.shape[1], pcd.shape[-2], pcd.shape[-1])
-        return self.pe(torch.cat((pcc_b, pcd), 1)).squeeze(1)
+        key_mask_cat = torch.cat((key_mask_src, key_mask_tgt), -1).unsqueeze(1)
+        P_src = torch.cat((P_src, torch.zeros_like(P_src)), 1)
+        P_tgt = torch.cat((P_tgt, torch.ones_like(P_tgt)), 1)
+        pcd = self.pf(torch.cat((P_src, P_tgt), -1))
+        y_cat = torch.cat((y_src, y_tgt), -1)
+        pcc = self.pn(torch.cat((pcd, y_cat), 1) * key_mask_cat).max(-1, keepdim=True)[0]
+        pcc_b = pcc.expand(pcc.shape[0], pcc.shape[1], y_cat.shape[-1])
+        return self.pe(torch.cat((pcc_b, pcd, y_cat), 1))[..., :y_src.shape[-1]]
 
     def forward(self, data_dict, **kwargs):
         src, tgt = data_dict['images']
@@ -107,13 +111,16 @@ class Net(nn.Module):
         for feat in self.encode(torch.cat([src, tgt])):
             feat_srcs.append(feat[:len(src)])
             feat_tgts.append(feat[len(src):])
-        F_src, F_tgt = self.align(feat_srcs, feat_tgts, P_src, P_tgt)
+        F_src, F_tgt = self.halo(feat_srcs, feat_tgts, P_src, P_tgt)
 
         y_src, y_tgt = self.cls(F_src), self.cls(F_tgt)
+        e_src = self.points(y_src, y_tgt, P_src, P_tgt, ns_src, ns_tgt)
+        e_tgt = self.points(y_tgt, y_src, P_tgt, P_src, ns_tgt, ns_src)
 
-        sim1 = self.points(y_src, y_tgt, P_src, P_tgt, ns_src, ns_tgt)
-        sim2 = self.points(y_tgt, y_src, P_tgt, P_src, ns_tgt, ns_src).transpose(1, 2)
-        
-        data_dict['ds_mat'] = self.sinkhorn(sim1 + sim2, ns_src, ns_tgt, dummy_row=True)
+        sim = torch.einsum(
+            "bci,bcj->bij",
+            e_src, e_tgt
+        )
+        data_dict['ds_mat'] = self.sinkhorn(sim, ns_src, ns_tgt, dummy_row=True)
         data_dict['perm_mat'] = hungarian(data_dict['ds_mat'], ns_src, ns_tgt)
         return data_dict
