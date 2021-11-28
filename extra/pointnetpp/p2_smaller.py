d45d4ae9a9d2657dd27f39141e09250e51d286ce
eliphatfs <flandre@scarletx.cn>
2021-11-28 23:12:04 +0800

    Remove projection head

--- extra/pointnetpp/p2_smaller.py
+++ extra/pointnetpp/p2_smaller.py
@@ -13,10 +13,7 @@ class get_model(nn.Module):
         self.sa3 = PointNetSetAbstraction(npoint=None, radius=None, nsample=None, in_channel=1024 + 3, mlp=[1024, 1024], group_all=True)
         self.fp3 = PointNetFeaturePropagation(in_channel=2048, mlp=[1024, 1024])
         self.fp1 = PointNetFeaturePropagation(in_channel=1030 + additional_channel, mlp=[512])
-        self.conv1 = nn.Conv1d(512, 128, 1)
-        self.bn1 = nn.BatchNorm1d(128)
-        self.drop1 = nn.Dropout(0.5)
-        self.conv2 = nn.Conv1d(128, 32, 1)
+        self.conv1 = nn.Conv1d(512, 32, 1)
 
     def forward(self, xyz):
         # Set Abstraction layers
@@ -33,9 +30,7 @@ class get_model(nn.Module):
         l1_points = self.fp3(l1_xyz, l3_xyz, l1_points, l3_points)
         l0_points = self.fp1(l0_xyz, l1_xyz, torch.cat([l0_xyz, l0_points], 1), l1_points)
         # FC layers
-        feat = F.relu(self.bn1(self.conv1(l0_points)))
-        x = self.drop1(feat)
-        x = self.conv2(x)
+        x = self.conv1(l0_points)
         # x = F.log_softmax(x, dim=1)
         # x = x.permute(0, 2, 1)
         return x
