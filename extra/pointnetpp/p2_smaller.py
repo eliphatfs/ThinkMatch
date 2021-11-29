4043e6fbca169acf87e56eb8eebde3cfbe023de9
eliphatfs <flandre@scarletx.cn>
2021-11-29 17:09:47 +0800

    Fix

--- extra/pointnetpp/p2_smaller.py
+++ extra/pointnetpp/p2_smaller.py
@@ -12,9 +12,9 @@ class get_model(nn.Module):
     def __init__(self, additional_channel, g_channel):
         super(get_model, self).__init__()
         self.normal_channel = True
-        self.sa1 = PointNetSetAbstractionMsg(24, [0.1, 0.2, 0.4], [24, 24, 24], 3 + additional_channel, [[64, 128], [128, 256], [64, 128]])
+        self.sa1 = PointNetSetAbstractionMsg(24, [0.1, 0.2, 0.3], [24, 24, 24], 3 + additional_channel, [[64, 128], [128, 256], [64, 128]])
         self.sa3 = PointNetSetAbstraction(npoint=None, radius=None, nsample=None, in_channel=512 + 3, mlp=[256, 1024], group_all=True)
-        self.fp3 = PointNetFeaturePropagation(in_channel=1024 + 128 + 256, mlp=[1024, 512])
+        self.fp3 = PointNetFeaturePropagation(in_channel=1024 + 128 + 256 + 128, mlp=[1024, 512])
         self.fp1 = PointNetFeaturePropagation(in_channel=518 + 32 + g_channel + additional_channel, mlp=[512, 256])
         self.conv1 = nn.Conv1d(256, 32, 1)
         self.cls_emb = nn.Embedding(len(labels), 32)
