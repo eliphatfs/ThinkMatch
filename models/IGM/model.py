3bb58a112198b9ecee36bf9754f0b057f680da88
eliphatfs <flandre@scarletx.cn>
2021-11-29 22:28:50 +0800

    Tooo slow

--- models/IGM/model.py
+++ models/IGM/model.py
@@ -59,11 +59,11 @@ class Net(nn.Module):
         # self.unet.load_state_dict(torch.load("unet_carvana_scale0.5_epoch1.pth"))
         feature_lat = 64 + (64 + 128 + 256 + 512 + 512 * 2)
         self.sconv = SiameseSConvOnNodes(feature_lat)
-        self.pix2pt_proj = ResCls(1, feature_lat, 512, 32)
+        self.pix2pt_proj = ResCls(1, feature_lat, 512, 48)
         self.pix2cl_proj = ResCls(1, 1024, 512, 128)
         self.tau = cfg.NGM.SK_TAU
         self.rescale = cfg.PROBLEM.RESCALE
-        self.pn = p2_smaller.get_model(32, 128)
+        self.pn = p2_smaller.get_model(48, 128)
         self.sinkhorn = Sinkhorn(
             max_iter=cfg.NGM.SK_ITER_NUM, tau=self.tau, epsilon=cfg.NGM.SK_EPSILON
         )
@@ -140,11 +140,11 @@ class Net(nn.Module):
         F_src, F_tgt, g_src, g_tgt = self.halo(feat_srcs, feat_tgts, P_src, P_tgt)
 
         G_src, G_tgt = data_dict['pyg_graphs']
-        G_src.x, G_tgt.x = batch_features(F_src, ns_src), batch_features(F_tgt, ns_tgt)
+        y_src, y_tgt = self.pix2pt_proj(F_src), self.pix2pt_proj(F_tgt)
+        G_src.x, G_tgt.x = batch_features(y_src, ns_src), batch_features(y_tgt, ns_tgt)
         F_src = unbatch_features(F_src, self.sconv(G_src).x, ns_src)
         F_tgt = unbatch_features(F_tgt, self.sconv(G_tgt).x, ns_tgt)
 
-        y_src, y_tgt = self.pix2pt_proj(F_src), self.pix2pt_proj(F_tgt)
         g_src, g_tgt = self.pix2cl_proj(g_src), self.pix2cl_proj(g_tgt)
         y_src, y_tgt = F.normalize(y_src, dim=1), F.normalize(y_tgt, dim=1)
         folding_src = self.points(y_src, P_src, ns_src, data_dict['cls'][0], g_src)
