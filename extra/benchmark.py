38621a8f06d294527a470eafc779c0406a27d5ee
eliphatfs <flandre@scarletx.cn>
2021-12-01 18:09:51 +0800

    Fix

--- extra/benchmark.py
+++ extra/benchmark.py
@@ -76,17 +76,17 @@ class Benchmark:
                     xmin, ymin, xmax, ymax = self.data_dict[keys]['bounds']
                     h = ymax - ymin
                     w = xmax - xmin
-                    rand_factor = 0.2 * min(
+                    rand_factor = min(
                         min(boundbox),
                         img.height - max(boundbox[1], boundbox[3]),
                         img.width - max(boundbox[0], boundbox[2]),
-                        h, w
+                        0.2 * h, 0.2 * w
                     )
                     x, y, x2, y2 = ((numpy.random.random([4]) - 0.5) * rand_factor).astype(numpy.int32)
                     xminn, yminn, xmaxn, ymaxn = (boundbox[0] + x, boundbox[1] + y, boundbox[2] + x2, boundbox[3] + y2)
                     for kpt in obj_dict['kpts']:
                         kpt['x'] = kpt['x'] / self.obj_resize[0] * w + xmin
-                        kpt['x'] = kpt['x'] / self.obj_resize[1] * h + ymin
+                        kpt['y'] = kpt['y'] / self.obj_resize[1] * h + ymin
                         kpt['x'] = (kpt['x'] - xminn) * self.obj_resize[0] / (xmaxn - xminn)
                         kpt['y'] = (kpt['y'] - yminn) * self.obj_resize[1] / (ymaxn - yminn)
                     obj = img.resize(self.obj_resize, resample=Image.BICUBIC,
