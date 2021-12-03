918701538295fa2afb25f64c015085b641c78a75
eliphatfs <flandre@scarletx.cn>
2021-12-03 22:57:12 +0800

    Smaller

--- extra/pointnetpp/p2_smaller.py
+++ extra/pointnetpp/p2_smaller.py
@@ -55,8 +55,8 @@ class get_model(nn.Module):
     def __init__(self, additional_channel, g_channel):
         super(get_model, self).__init__()
         self.normal_channel = True
-        self.sa1 = PointNetSetAbstractionMsg(48, [0.1, 0.2, 0.3, 0.6, 1.0], [48] * 5, 3 + additional_channel, [[64, 128], [128, 256], [64, 128], [32, 64], [24, 48]])
-        self.sa2 = PointNetSetAbstractionMsg(48, [0.1, 0.2, 0.3, 0.6, 1.0], [48] * 5, 512 + 64 + 48, [[96, 128], [192, 256], [96, 128], [48, 64], [36, 48]])
+        self.sa1 = PointNetSetAbstractionMsg(36, [0.1, 0.2, 0.3, 0.6, 1.0], [36] * 5, 3 + additional_channel, [[64, 128], [128, 256], [64, 128], [32, 64], [24, 48]])
+        self.sa2 = PointNetSetAbstractionMsg(36, [0.1, 0.2, 0.3, 0.6, 1.0], [36] * 5, 512 + 64 + 48, [[96, 128], [192, 256], [96, 128], [48, 64], [36, 48]])
         self.sa3 = PointNetSetAbstraction(npoint=None, radius=None, nsample=None, in_channel=512 + 64 + 48 + 3, mlp=[300, 1024], group_all=True)
         self.fp3 = PointNetFeaturePropagation(in_channel=1024 + 128 + 256 + 128 + 64 + 48, mlp=[1024, 512])
         self.fp2 = PointNetFeaturePropagation(in_channel=512 + 128 + 256 + 128 + 64 + 48, mlp=[512, 384])
