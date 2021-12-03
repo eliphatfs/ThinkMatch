fb90fbaca1c78c860da816a745600ebaaf9a2cb7
eliphatfs <flandre@scarletx.cn>
2021-12-03 15:04:03 +0800

    Rewind

--- models/IGM/model.py
+++ models/IGM/model.py
@@ -52,47 +52,21 @@ def unbatch_features(orig, embeddings, num_vertices):
     return res
 
 
-class HALO(nn.Module):
-    def __init__(self, sinkhorn, n_emb, rescale):
-        super().__init__()
-        self.rescale = rescale
-        self.sinkhorn = sinkhorn
-        self.projection_g = ResCls(1, 1024, 256, 128)
-        self.pp2 = p2_smaller.MultiScalePropagation(n_emb, 128, n_emb)
-        self.attn = p2_smaller.HALOAttention(sinkhorn, ResCls(0, n_emb, n_emb * 2, n_emb), n_emb, 36)
-
-    def prepare_p(self, y_src, P_src, n_src):
-        resc = P_src.new_tensor(self.rescale)
-        P_src = (P_src - P_src.min(1, keepdim=True)[0]) / (P_src.max(1, keepdim=True)[0] - P_src.min(1, keepdim=True)[0] + 1e-3)
-        # P_src = P_src / resc
-        P_src = P_src.transpose(1, 2)
-        key_mask_src = torch.arange(y_src.shape[-1], device=n_src.device).expand(len(y_src), y_src.shape[-1]) < n_src.unsqueeze(-1)
-        P_src = torch.cat((P_src, torch.zeros_like(P_src[:, :1])), 1)
-        cat = torch.cat((P_src, y_src), 1) * key_mask_src.unsqueeze(1)
-        return cat
-
-    def forward(self, x_src, x_tgt, P_src, P_tgt, g_src, g_tgt, n_src, n_tgt):
-        g_src, g_tgt = self.projection_g(g_src), self.projection_g(g_tgt)
-        g_src, g_tgt = F.normalize(g_src, 1), F.normalize(g_tgt, 1)
-        x_src, x_tgt = self.prepare_p(x_src, P_src, n_src), self.prepare_p(x_tgt, P_tgt, n_tgt)
-        x_src, x_tgt = self.pp2(x_src, g_src), self.pp2(x_tgt, g_tgt)
-        return self.attn(x_src, x_tgt, n_src, n_tgt)
-
-
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.resnet = resnet34(True)
+        self.resnet = resnet34(True)  # UNet(3, 2)
+        # self.unet.load_state_dict(torch.load("unet_carvana_scale0.5_epoch1.pth"))
         feature_lat = 64 + (64 + 128 + 256 + 512 + 512 * 2)
-        self.pix2pt_proj = ResCls(1, feature_lat, 1024, 384)
+        # self.sconv = SiameseSConvOnNodes(48)
+        self.pix2pt_proj = ResCls(1, feature_lat, 512, 64)
+        self.pix2cl_proj = ResCls(1, 1024, 512, 128)
         self.tau = cfg.IGM.SK_TAU
         self.rescale = cfg.PROBLEM.RESCALE
+        self.pn = p2_smaller.get_model(64, 128)
         self.sinkhorn = Sinkhorn(
             max_iter=cfg.IGM.SK_ITER_NUM, tau=self.tau, epsilon=cfg.IGM.SK_EPSILON
         )
-        self.final_proj = ResCls(1, 384, 384, 36)
-        self.halo_1 = HALO(self.sinkhorn, 384, self.rescale)
-        self.halo_2 = HALO(self.sinkhorn, 384, self.rescale)
         self.backbone_params = list(self.resnet.parameters())
 
     @property
@@ -118,7 +92,7 @@ class Net(nn.Module):
         x = r.avgpool(x)
         yield x
 
-    def merge_feat(self, feat_srcs, feat_tgts, P_src, P_tgt):
+    def halo(self, feat_srcs, feat_tgts, P_src, P_tgt):
         U_src = torch.cat([
             my_align(feat_src, P_src, self.rescale) for feat_src in feat_srcs
         ], 1)
@@ -139,28 +113,47 @@ class Net(nn.Module):
         ghalo_tgt = torch.cat((glob_tgt, glob_src), 1)
         return F_src, F_tgt, ghalo_src, ghalo_tgt
 
+    def points(self, y_src, P_src, n_src, cls, g):
+        resc = P_src.new_tensor(self.rescale)
+        P_src = P_src / resc
+        P_src = P_src.transpose(1, 2)
+        # if self.training:
+        #     P_src = P_src + torch.rand_like(P_src[..., :1]) * 0.06 - 0.03
+        key_mask_src = torch.arange(y_src.shape[-1], device=n_src.device).expand(len(y_src), y_src.shape[-1]) < n_src.unsqueeze(-1)
+        P_src = torch.cat((P_src, torch.zeros_like(P_src[:, :1])), 1)
+        return self.pn(torch.cat((P_src, y_src), 1) * key_mask_src.unsqueeze(1), cls, g)[..., :y_src.shape[-1]]
+
     def forward(self, data_dict, **kwargs):
         src, tgt = data_dict['images']
         P_src, P_tgt = data_dict['Ps']
         ns_src, ns_tgt = data_dict['ns']
-        self.sinkhorn.batched_operation = not self.training
 
         feat_srcs, feat_tgts = [], []
         for feat in self.encode(torch.cat([src, tgt])):
             feat_srcs.append(feat[:len(src)])
             feat_tgts.append(feat[len(src):])
+        # resc = P_src.new_tensor(self.rescale)
+        # rand_src, rand_tgt = torch.rand(len(P_src), 64, 2).to(P_src), torch.rand(len(P_tgt), 64, 2).to(P_tgt)
+        # P_src, P_tgt = torch.cat((rand_src * resc, P_src), 1), torch.cat((rand_tgt * resc, P_tgt), 1)
         if self.training:
-            # P_src = P_src + torch.randn_like(P_src)
-            # P_tgt = P_tgt + torch.randn_like(P_tgt)
-            P_src = P_src + torch.rand_like(P_src[..., :1]) * 10 - 5
-            P_tgt = P_tgt + torch.rand_like(P_tgt[..., :1]) * 10 - 5
-        F_src, F_tgt, g_src, g_tgt = self.merge_feat(feat_srcs, feat_tgts, P_src, P_tgt)
+            P_src = P_src + torch.rand_like(P_src) * 2 - 1
+            P_tgt = P_tgt + torch.rand_like(P_tgt) * 2 - 1
+        F_src, F_tgt, g_src, g_tgt = self.halo(feat_srcs, feat_tgts, P_src, P_tgt)
 
+        # G_src, G_tgt = data_dict['pyg_graphs']
         y_src, y_tgt = self.pix2pt_proj(F_src), self.pix2pt_proj(F_tgt)
+        # y_src = self.pix2pt_norm(y_src)
+        # y_tgt = self.pix2pt_norm(y_tgt)
+        # G_src.x, G_tgt.x = batch_features(y_src, ns_src), batch_features(y_tgt, ns_tgt)
+        # y_src = unbatch_features(y_src, self.sconv(G_src).x, ns_src)
+        # y_src = unbatch_features(y_src, self.sconv(G_tgt).x, ns_tgt)
+
+        g_src, g_tgt = self.pix2cl_proj(g_src), self.pix2cl_proj(g_tgt)
         y_src, y_tgt = F.normalize(y_src, dim=1), F.normalize(y_tgt, dim=1)
-        y_src, y_tgt = self.halo_1(y_src, y_tgt, P_src, P_tgt, g_src, g_tgt, ns_src, ns_tgt)
-        y_src, y_tgt = self.halo_2(y_src, y_tgt, P_src, P_tgt, g_src, g_tgt, ns_src, ns_tgt)
-        folding_src, folding_tgt = self.final_proj(y_src), self.final_proj(y_tgt)
+        g_src, g_tgt = F.normalize(g_src, dim=1), F.normalize(g_tgt, dim=1)
+        folding_src = self.points(y_src, P_src, ns_src, data_dict['cls'][0], g_src)
+        folding_tgt = self.points(y_tgt, P_tgt, ns_tgt, data_dict['cls'][1], g_tgt)
+
         sim = torch.einsum(
             'bci,bcj->bij',
             folding_src,
