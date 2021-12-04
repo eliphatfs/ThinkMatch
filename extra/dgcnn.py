1f456543691189df718448f43cdb58f1f3347595
eliphatfs <flandre@scarletx.cn>
2021-12-04 23:00:10 +0800

    Everyone sixty-four

--- extra/dgcnn.py
+++ extra/dgcnn.py
@@ -61,8 +61,8 @@ class DGCNN(nn.Module):
         
         self.bn1 = nn.BatchNorm2d(64)
         # self.bn2 = nn.BatchNorm2d(64)
-        self.bn3 = nn.BatchNorm2d(128)
-        self.bn4 = nn.BatchNorm2d(256)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.bn4 = nn.BatchNorm2d(64)
         self.bn5 = nn.BatchNorm1d(emb_dims)
 
         self.conv1 = nn.Sequential(nn.Conv2d(6 + extra_dims * 2, 64, kernel_size=1, bias=False),
@@ -71,13 +71,13 @@ class DGCNN(nn.Module):
         # self.conv2 = nn.Sequential(nn.Conv2d(64*2, 64, kernel_size=1, bias=False),
         #                            self.bn2,
         #                            nn.LeakyReLU(negative_slope=0.2))
-        self.conv3 = nn.Sequential(nn.Conv2d(64*2, 128, kernel_size=1, bias=False),
+        self.conv3 = nn.Sequential(nn.Conv2d(64*2, 64, kernel_size=1, bias=False),
                                    self.bn3,
                                    nn.LeakyReLU(negative_slope=0.2))
-        self.conv4 = nn.Sequential(nn.Conv2d(128*2, 256, kernel_size=1, bias=False),
+        self.conv4 = nn.Sequential(nn.Conv2d(64*2, 64, kernel_size=1, bias=False),
                                    self.bn4,
                                    nn.LeakyReLU(negative_slope=0.2))
-        self.conv5 = nn.Sequential(nn.Conv1d(448 + g_dims, emb_dims, kernel_size=1, bias=False),
+        self.conv5 = nn.Sequential(nn.Conv1d(192 + g_dims, emb_dims, kernel_size=1, bias=False),
                                    self.bn5,
                                    nn.LeakyReLU(negative_slope=0.2))
         # self.linear1 = nn.Linear(args.emb_dims*2, 512, bias=False)
