55ac49e7e80e16f52bbce9ad30537e2c1d412066
eliphatfs <flandre@scarletx.cn>
2021-12-05 12:15:31 +0800

    Fix

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 import torch.optim as optim
 import time
+from src.dataset import data_loader
 import xlwt
 from datetime import datetime
 from pathlib import Path
@@ -59,20 +60,53 @@ def spot_norm():
 
 
 def spot_out_minmax():
+    dataset_len = {'train': cfg.TRAIN.EPOCH_ITERS * cfg.BATCH_SIZE, 'test': cfg.EVAL.SAMPLES}
+    ds_dict = cfg[cfg.DATASET_FULL_NAME] if ('DATASET_FULL_NAME' in cfg) and (cfg.DATASET_FULL_NAME in cfg) else {}
+    benchmark = {
+        x: Benchmark(name=cfg.DATASET_FULL_NAME,
+                     sets=x,
+                     problem=cfg.PROBLEM.TYPE,
+                     obj_resize=cfg.PROBLEM.RESCALE,
+                     filter=cfg.PROBLEM.FILTER,
+                     **ds_dict)
+        for x in ('train', 'test')}
+    image_dataset = {
+        x: GMDataset(name=cfg.DATASET_FULL_NAME,
+                     bm=benchmark[x],
+                     problem=cfg.PROBLEM.TYPE,
+                     length=dataset_len[x],
+                     cls=cfg.TRAIN.CLASS if x == 'train' else cfg.EVAL.CLASS,
+                     using_all_graphs=cfg.PROBLEM.TRAIN_ALL_GRAPHS if x == 'train' else cfg.PROBLEM.TEST_ALL_GRAPHS)
+        for x in ('train', 'test')}
+    dataloader = {x: get_dataloader(image_dataset[x], shuffle=True, fix_seed=(x == 'test'), batch_size=cfg.BATCH_SIZE if (x != 'test') else 32)
+                  for x in ('train', 'test')}
     cfg_from_file("experiments/igm.yaml")
     net = Net()
     kv = collections.defaultdict(list)
     load_model(net, "output/igm_voc/params/params_%04d.pt" % 10)
+    net.eval()
     spot_hook(net, kv)
-    plotlib.figure(figsize=[60, 18])
-    kvs = []
-    labels = []
-    for k, v in kv.items():
-        kvs.extend(numpy.array(v).T)
-        labels.extend([k + x for x in [".mean", ".std", ".min", ".max"]])
-    import pdb; pdb.set_trace()
-    plotlib.boxplot(numpy.array(kvs).T, labels=labels)
-    plotlib.legend()
+    plotlib.figure(figsize=[54, 20])
+
+    def _do(split):
+        for i, inputs in enumerate(dataloader[split]):
+            if i >= 10:
+                break
+            inputs = data_to_cuda(inputs)
+            with torch.no_grad():
+                net(inputs)
+        kvs = []
+        labels = []
+        for k, v in kv.items():
+            kvs.extend(numpy.array(v).T)
+            labels.extend([k + x for x in [".mean", ".std", ".min", ".max"]])
+        plotlib.boxplot(numpy.array(kvs).T, labels=labels)
+        plotlib.xlabel(split)
+
+    plotlib.subplot(2, 1, 1)
+    _do("train")
+    plotlib.subplot(2, 1, 2)
+    _do("test")
     plotlib.savefig("spot.png")
 
 if __name__ == "__main__":
