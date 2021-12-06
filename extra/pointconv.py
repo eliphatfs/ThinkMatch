747c9d3da9d2b5204d35518fa6b105ab3a3c4382
eliphatfs <flandre@scarletx.cn>
2021-12-06 20:36:37 +0800

    Fix

--- extra/pointconv.py
+++ extra/pointconv.py
@@ -291,62 +291,6 @@ class WeightNet(nn.Module):
         return weights
 
 
-class PointConvSetAbstraction(nn.Module):
-    def __init__(self, npoint, nsample, in_channel, mlp, group_all):
-        super(PointConvSetAbstraction, self).__init__()
-        self.npoint = npoint
-        self.nsample = nsample
-        self.mlp_convs = nn.ModuleList()
-        self.mlp_bns = nn.ModuleList()
-        last_channel = in_channel
-        for out_channel in mlp:
-            self.mlp_convs.append(nn.Conv2d(last_channel, out_channel, 1))
-            self.mlp_bns.append(nn.BatchNorm2d(out_channel))
-            last_channel = out_channel
-
-        self.weightnet = WeightNet(3, 16)
-        self.linear = nn.Linear(16 * mlp[-1], mlp[-1])
-        self.bn_linear = nn.BatchNorm1d(mlp[-1])
-        self.group_all = group_all
-
-    def forward(self, xyz, points):
-        """
-        Input:
-            xyz: input points position data, [B, C, N]
-            points: input points data, [B, D, N]
-        Return:
-            new_xyz: sampled points position data, [B, C, S]
-            new_points_concat: sample points feature data, [B, D', S]
-        """
-        B = xyz.shape[0]
-        xyz = xyz.permute(0, 2, 1)
-        if points is not None:
-            points = points.permute(0, 2, 1)
-
-        if self.group_all:
-            new_xyz, new_points, grouped_xyz_norm = sample_and_group_all(xyz, points)
-        else:
-            new_xyz, new_points, grouped_xyz_norm, _ = sample_and_group(self.npoint, self.nsample, xyz, points)
-        # new_xyz: sampled points position data, [B, npoint, C]
-        # new_points: sampled points data, [B, npoint, nsample, C+D]
-        new_points = new_points.permute(0, 3, 2, 1)  # [B, C+D, nsample,npoint]
-        for i, conv in enumerate(self.mlp_convs):
-            bn = self.mlp_bns[i]
-            new_points = F.relu(bn(conv(new_points)))
-
-        grouped_xyz = grouped_xyz_norm.permute(0, 3, 2, 1)
-        weights = self.weightnet(grouped_xyz)
-        new_points = torch.matmul(input=new_points.permute(0, 3, 1, 2), other=weights.permute(0, 3, 2, 1)).view(B,
-                                                                                                                self.npoint,
-                                                                                                                -1)
-        new_points = self.linear(new_points)
-        new_points = self.bn_linear(new_points.permute(0, 2, 1))
-        new_points = F.relu(new_points)
-        new_xyz = new_xyz.permute(0, 2, 1)
-
-        return new_xyz, new_points
-
-
 class PointConvDensitySetAbstraction(nn.Module):
     def __init__(self, npoint, nsample, in_channel, mlp, bandwidth, group_all):
         super(PointConvDensitySetAbstraction, self).__init__()
@@ -404,7 +348,7 @@ class PointConvDensitySetAbstraction(nn.Module):
         grouped_xyz = grouped_xyz * grouped_density.permute(0, 3, 2, 1)
         weights = self.weightnet(grouped_xyz)
         new_points = torch.matmul(input=new_points.permute(0, 3, 1, 2), other=weights.permute(0, 3, 2, 1)).view(B,
-                                                                                                                self.npoint,
+                                                                                                                min(self.npoint, N),
                                                                                                                 -1)
         new_points = self.linear(new_points)
         new_points = self.bn_linear(new_points.permute(0, 2, 1))
@@ -493,7 +437,7 @@ class PointConv(nn.Module):
         # self.fp1 = PointConvDensityFeaturePropogation(nsample=16, in_channel=512+3,  concat_channel=256, bandwidth=0.8, mlp=[512, 512])
         self.fp2 = PointConvDensityFeaturePropogation(nsample=16, in_channel=512+3, concat_channel=128, bandwidth=0.3, mlp=[256, 256])
         self.fp3 = PointConvDensityFeaturePropogation(nsample=16, in_channel=256+3, concat_channel=64, bandwidth=0.2, mlp=[256, 128])
-        self.fp4 = PointConvDensityFeaturePropogation(nsample=16, in_channel=128+3+g_channel, concat_channel=3, bandwidth=0.1, mlp=[128, 64, 64])
+        self.fp4 = PointConvDensityFeaturePropogation(nsample=16, in_channel=128+3+g_channel+i_channel, concat_channel=3, bandwidth=0.1, mlp=[128, 64, 64])
 
         self.mlp = nn.Conv1d(64, 32, 1)
 
