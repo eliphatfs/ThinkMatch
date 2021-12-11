085c3ef6cee7930d0b50da96cc1b1fc737e09207
eliphatfs <flandre@scarletx.cn>
2021-12-11 13:28:18 +0800

    Fix

--- models/NGM/model_v3.py
+++ models/NGM/model_v3.py
@@ -36,7 +36,7 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.backbone = Backbone()
         self.backbone_params = self.backbone.backbone_params
-        self.message_pass_node_features = SiameseSConvOnNodes(input_node_dim=1536)
+        self.message_pass_node_features = SiameseSConvOnNodes(input_node_dim=256)
         self.build_edge_features_from_node_features = SiameseNodeFeaturesToEdgeFeatures(
             total_num_nodes=self.message_pass_node_features.num_node_features
         )
